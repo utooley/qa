58530bb77209fcce4039a4d318b45f2517993d6d
Ursula Tooley <utooley@gmail.com>
2017-04-09 19:53:34 -0400

    added control non-homolog connectivity to R code

--- aparc_homologs_connectivity_sanity_check.R
+++ aparc_homologs_connectivity_sanity_check.R
@@ -22,3 +22,29 @@ pericalcarinehomo.df = as.data.frame(do.call(rbind,pericalcarinehomo))
 df=cbind(precentralhomo.df,postcentralhomo.df,occipitalhomo.df,pericalcarinehomo.df)
 
 write.csv(df, "aparc_roi_homolog_correlations.csv", row.names=TRUE)
+
+
+# Non-Homolog Control ROIs ------------------------------------------------
+
+precentrallatoccipitalb <- list()
+postcentralprecuneusb <- list()
+occipitalmidtemporall <- list()
+precentralpericalcarineb<-list()
+for (s in subjects[,1]){
+  mine <- sprintf("%s_rest_aparc_meants.csv", s)
+  df <- read.csv(file=mine)
+  table <- cor(df)
+  precentrallatoccipitalb[[s]] <- table[1,11]
+  postcentralprecuneusb[[s]] <- table[3,8]
+  occipitalmidtemporall[[s]] <- table[9,4]
+  precentralpericalcarineb[[s]]<-table[1,10]         
+}
+
+precentrallatoccipitalb.df = as.data.frame(do.call(rbind, precentrallatoccipitalb))
+postcentralprecuneusb.df = as.data.frame(do.call(rbind, postcentralprecuneusb))
+occipitalmidtemporall.df = as.data.frame(do.call(rbind,occipitalmidtemporall))
+precentralpericalcarineb.df = as.data.frame(do.call(rbind,precentralpericalcarineb))
+df=cbind(precentrallatoccipitalb.df,postcentralprecuneusb.df,occipitalmidtemporall.df,precentralpericalcarineb.df)
+
+write.csv(df, "aparc_roi_control_nonhomolog_correlations.csv", row.names=TRUE)
+
